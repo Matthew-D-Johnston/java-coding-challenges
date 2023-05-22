5cb1f9ce5e917552eac45a26f3e13c3c262aaca9
Matthew-D-Johnston <49563465+Matthew-D-Johnston@users.noreply.github.com>
2023-05-21 22:25:36 -0300

    Add Java exercise files.

---
+++ edabit/very-easy/how_edabit_works.java
@@ -0,0 +1,9 @@
+public class how_edabit_works {
+  public static boolean returnTrue() {
+    return true;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(returnTrue());
+  }
+}
